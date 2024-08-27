dc74bfa4b099081797699c813d59f7730ccf7aac
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-28 00:43:37 +0330

    Publisher Class

---
+++ OOP/Library/Publisher.java
@@ -0,0 +1,15 @@
+public class Publisher
+{
+    private String name;
+
+    public Publisher(String name)
+    {
+        this.name=name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+}
