6d9a896b5336bbe432748c5c7b6108788ad70477
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-27 20:54:44 +0330

    Librarian extends User

---
+++ OOP/Library/Librarian.java
@@ -0,0 +1,15 @@
+public class Librarian extends User
+{
+    public Librarian(String id, String name, String email)
+    {
+        super(id, name, email);
+    }
+    @Override
+    public void login()
+    {
+
+    }
+    @Override
+    public void logout()
+    {}
+}
--- OOP/Library/Patron.java
+++ OOP/Library/Patron.java
@@ -4,4 +4,5 @@ public class Patron extends User{
     {
         super(id, name, email);
     }
+
 }
