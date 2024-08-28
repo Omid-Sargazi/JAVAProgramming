3b99b521035841cece44c978d529791d1e6b500c
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-29 01:23:47 +0330

    two product for Computer Factory

---
+++ AdvancedProgramming/FactoryPattern/ComputerFactory/Desktop.java
@@ -0,0 +1,9 @@
+package AdvancedProgramming.FactoryPattern.ComputerFactory;
+
+public class Desktop implements Computer {
+    @Override
+    public void boot() {
+        System.out.println("Booting Desktop");
+
+    }
+}
---
+++ AdvancedProgramming/FactoryPattern/ComputerFactory/Laptop.java
@@ -0,0 +1,9 @@
+package AdvancedProgramming.FactoryPattern.ComputerFactory;
+
+public class Laptop implements Computer {
+    @Override
+    public void boot() {
+        System.out.println("Booting Laptop");
+
+    }
+}
