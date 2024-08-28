ae9fe32e39e06f1f6c59b9fcdbb143d6ea6befa1
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-28 22:26:11 +0330

    built ShapeFactory for creating every shape

---
+++ AdvancedProgramming/FactoryPattern/ShapeFactory/ShapeFactory.java
@@ -0,0 +1,12 @@
+package AdvancedProgramming.FactoryPattern.ShapeFactory;
+
+public class ShapeFactory {
+    public Shape getShape(String shapeType) {
+        if (shapeType.equalsIgnoreCase("circle")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("rectangle")) {
+            return new Rectangle();
+        }
+        return null;
+    }
+}
