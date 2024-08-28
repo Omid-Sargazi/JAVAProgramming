c4c57435fa2491723ac661c1c515fdd1ccfdbf2d
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-29 00:54:57 +0330

    two food for factory

---
+++ AdvancedProgramming/FactoryPattern/FoodFactory/Burger.java
@@ -0,0 +1,9 @@
+package AdvancedProgramming.FactoryPattern.FoodFactory;
+
+public class Burger implements Food {
+    @Override
+    public void eat() {
+        System.out.println("Eating Burger");
+
+    }
+}
---
+++ AdvancedProgramming/FactoryPattern/FoodFactory/Pizza.java
@@ -0,0 +1,9 @@
+package AdvancedProgramming.FactoryPattern.FoodFactory;
+
+public class Pizza implements Food {
+    @Override
+    public void eat() {
+        System.out.println("Eating Pizza");
+
+    }
+}
