1e256d34ee7f354dd41c2f069c09b0074bc51a17
Omid-Saegazi <om.sargazi@gmail.com>
2024-08-28 22:22:35 +0330

    Two shapes implement the interface

---
+++ AdvancedProgramming/FactoryPattern/ShapeFactory/Rectangle.java
@@ -0,0 +1,9 @@
+package AdvancedProgramming.FactoryPattern.ShapeFactory;
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Rectangle");
+
+    }
+}
