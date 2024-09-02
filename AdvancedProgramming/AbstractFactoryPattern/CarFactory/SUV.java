687ac67e37a16c191638dd5084c4befe0bdc85a3
Omid-Saegazi <om.sargazi@gmail.com>
2024-09-02 18:08:59 +0330

    classes

---
+++ AdvancedProgramming/AbstractFactoryPattern/CarFactory/SUV.java
@@ -0,0 +1,8 @@
+package AdvancedProgramming.AbstractFactoryPattern.CarFactory;
+
+public class SUV implements Car {
+    public void drive() {
+        System.out.println("Driving an SUV");
+
+    }
+}
---
+++ AdvancedProgramming/AbstractFactoryPattern/CarFactory/Sedan.java
@@ -0,0 +1,8 @@
+package AdvancedProgramming.AbstractFactoryPattern.CarFactory;
+
+public class Sedan implements Car {
+    public void drive() {
+        System.out.println("Driving a Sedan");
+
+    }
+}
