b6ddb6faf75744c835ee20c4b74eacf8f2314eab
Omid-Saegazi <om.sargazi@gmail.com>
2024-09-02 17:38:54 +0330

    Victorian Furniture Factory

---
+++ AdvancedProgramming/AbstractFactoryPattern/FurnitureFactory03/VictorianFurnitureFactory.java
@@ -0,0 +1,13 @@
+package AdvancedProgramming.AbstractFactoryPattern.FurnitureFactory03;
+
+public class VictorianFurnitureFactory implements FurnitureFactory {
+    @Override
+    public Chair createChair() {
+        return new VictorianChair();
+    }
+
+    @Override
+    public Table createTable() {
+        return new VictorianTable();
+    }
+}
