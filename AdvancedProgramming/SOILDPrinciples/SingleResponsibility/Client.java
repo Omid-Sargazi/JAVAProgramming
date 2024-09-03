4e3fb24cb438a7f85332ff4222da013de5799548
Omid-Saegazi <om.sargazi@gmail.com>
2024-09-03 23:51:30 +0330

    Usage without SRP

--- AdvancedProgramming/SOILDPrinciples/SingleResponsibility/Client.java
+++ AdvancedProgramming/SOILDPrinciples/SingleResponsibility/Client.java
@@ -3,5 +3,19 @@ package AdvancedProgramming.SOILDPrinciples.SingleResponsibility;
 public class Client {
     public static void main(String[] args) {
         System.out.println("*** A demo without SRP.***");
+
+        Employee robin = new Employee("Rabin", "Smith", 7.5);
+        showEmpDetail(robin);
+        System.out.println("\n*******\n");
+
+        Employee Kevin = new Employee("Kevin", "Proctor", 3.2);
+        showEmpDetail(Kevin);
+
+    }
+
+    private static void showEmpDetail(Employee emp) {
+        emp.displayEmpDetail();
+        System.out.println("The employee Id: " + emp.generateEmpId(emp.firstName));
+        System.out.println("This employee is a " + emp.checkSeniority(emp.experienceInYears) + " employee");
     }
 }
