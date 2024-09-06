5c9303f5eb6583580164b1a3f2d1539298f195e4
Omid-Saegazi <om.sargazi@gmail.com>
2024-09-06 22:51:26 +0330

    RegisteredUserPayment class implement from payment

---
+++ AdvancedProgramming/LiskovSubstitutionPrinciple/RegisteredUserPayment.java
@@ -0,0 +1,20 @@
+package AdvancedProgramming.LiskovSubstitutionPrinciple;
+
+public class RegisteredUserPayment implements Payment {
+    String name;
+
+    public RegisteredUserPayment(String userName) {
+        this.name = userName;
+    }
+
+    @Override
+    public void previousPaymentInfo() {
+        System.out.println("Retrieving " + name + "'s last payment details.");
+    }
+
+    @Override
+    public void newPayment() {
+        System.out.println("Processing new payment for " + name + ".");
+    }
+
+}
