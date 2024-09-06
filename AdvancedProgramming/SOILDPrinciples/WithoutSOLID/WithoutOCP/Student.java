82201068174a045a645b08e905e4166474e2f604
Omid-Saegazi <om.sargazi@gmail.com>
2024-09-06 20:17:39 +0330

    Student class without SOLID principles

---
+++ AdvancedProgramming/SOILDPrinciples/WithoutSOLID/WithoutOCP/Student.java
@@ -0,0 +1,22 @@
+package AdvancedProgramming.SOILDPrinciples.WithoutSOLID.WithoutOCP;
+
+public class Student {
+    String name;
+    String regNumber;
+    String department;
+    double score;
+
+    public Student(String name, String regNumber, String department, double score) {
+        this.name = name;
+        this.regNumber = regNumber;
+        this.department = department;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [name=" + name + ", regNumber=" + regNumber + ", department=" + department + ", score=" + score
+                + "]";
+    }
+
+}
