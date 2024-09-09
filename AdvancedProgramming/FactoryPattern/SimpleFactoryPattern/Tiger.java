26728b65471b1146b4939cbffbbc4b84bb95cdaf
Omid-Saegazi <om.sargazi@gmail.com>
2024-09-10 01:35:54 +0330

    Tiger class implements animal

---
+++ AdvancedProgramming/FactoryPattern/SimpleFactoryPattern/Tiger.java
@@ -0,0 +1,12 @@
+package AdvancedProgramming.FactoryPattern.SimpleFactoryPattern;
+
+public class Tiger implements Animal {
+    public Tiger() {
+        System.out.println("\nA tiger is created.");
+    }
+
+    public void displayBehavior() {
+        System.out.println("Tiger says: Halum.");
+        System.out.println("It loves to roam in a jungle.");
+    }
+}
