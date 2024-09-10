2355259cb1bdaa5ae0596600854eda47217b30e5
Omid-Saegazi <om.sargazi@gmail.com>
2024-09-11 00:06:06 +0330

    create class and initial variables

---
+++ FunctionalProgramming/Projects/ecosystem/EcosystemSimulation.java
@@ -0,0 +1,15 @@
+package FunctionalProgramming.Projects.ecosystem;
+
+import java.util.Random;
+
+public class EcosystemSimulation {
+    private static final int RIVER_SIZE = 10;
+    private static final int INITIAL_BEAR_COUNT = 3;
+    private static final int INITIAL_FISH_COUNT = 3;
+    private static Animal[] river = new Animal[RIVER_SIZE];
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+
+    }
+}
