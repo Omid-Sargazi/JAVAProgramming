2ed48b605f1ac7d87228b21144393e8d6783ed6a
Omid-Saegazi <om.sargazi@gmail.com>
2024-09-11 00:32:54 +0330

    Separate class check with SRP

---
+++ AdvancedProgramming/ProgramWIthISP/SeniorityChecker.java
@@ -0,0 +1,7 @@
+package AdvancedProgramming.ProgramWIthISP;
+
+public class SeniorityChecker {
+    public String checkSeniority(double experienceInYears) {
+        return experienceInYears > 5 ? "senior" : "junior";
+    }
+}
